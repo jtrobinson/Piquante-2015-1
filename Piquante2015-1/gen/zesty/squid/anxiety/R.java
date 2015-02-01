ebc73724dd80f1b7152bd4fda05d975485e07f17
JustinR Home PC <jtrobinson629@gmail.com>
2015-02-01 18:10:15 -0500

    Layout is acceptable!
    I'm sure I'll need to fudge it more later, but the proportions are okay
    for my current purposes,

--- Piquante2015-1/gen/zesty/squid/anxiety/R.java
+++ Piquante2015-1/gen/zesty/squid/anxiety/R.java
@@ -28,16 +28,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
+        public static final int action_settings=0x7f09000e;
+        public static final int bottomBar=0x7f090009;
         public static final int btnOptions=0x7f090003;
         public static final int btnStartGame=0x7f090002;
         public static final int btnStartGameLayout=0x7f090001;
         public static final int btnXMLExample=0x7f090004;
-        public static final int btn_get_involved=0x7f090006;
-        public static final int btn_get_involved_below=0x7f090008;
-        public static final int game_screen_sceneview=0x7f090005;
+        public static final int btn_get_involved=0x7f09000b;
+        public static final int btn_get_involved_below=0x7f09000d;
+        public static final int gameSceneContainer=0x7f090007;
+        public static final int game_screen_sceneview=0x7f090008;
+        public static final int label1=0x7f090006;
+        public static final int label2=0x7f09000a;
         public static final int textView1=0x7f090000;
-        public static final int xmllayoutexample_rendersurfaceview=0x7f090007;
+        public static final int upperBar=0x7f090005;
+        public static final int xmllayoutexample_rendersurfaceview=0x7f09000c;
     }
     public static final class layout {
         public static final int act_game_screen=0x7f030000;
